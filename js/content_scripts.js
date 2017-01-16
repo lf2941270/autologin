7601ffe5e484f04aa318dfbce9964a4ab83ed4ec
李凡 <454730788@qq.com>
2017-01-16 11:23:52 +0800

    移除不必要的操作

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -1,7 +1,7 @@
 function autoLogin() {
     $('#login_text_username').val('admin');
     $('#login_text_password').val('admin@161231');
-    $('#login_text_verifycode').prop('maxlength', '').attr('maxlength', '').removeClass('ng-valid-maxlength').removeClass('ng-valid').val('@fw$')
+    $('#login_text_verifycode').val('@fw$');
     setTimeout(function(){
         document.querySelector("#login_text_username").dispatchEvent(new Event('change'));
         document.querySelector("#login_text_password").dispatchEvent(new Event('change'));
