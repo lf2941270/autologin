b158625bba82686a2c08ec735bcc2ea9a48a3ab8
李凡 <fan.li@acorn-net.com>
2017-01-16 11:20:04 +0800

    更新验证码

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -1,14 +1,16 @@
 function autoLogin() {
     $('#login_text_username').val('admin');
     $('#login_text_password').val('admin@161231');
-    $('#login_text_verifycode').val('@c0rnC0d$').change()
-    $('#login_text_verifycode').trigger('input');
-    document.querySelector("#login_text_username").dispatchEvent(new Event('change'));
-    document.querySelector("#login_text_password").dispatchEvent(new Event('change'));
-    document.querySelector("#login_text_verifycode").dispatchEvent(new Event('change'));
+    $('#login_text_verifycode').prop('maxlength', '').attr('maxlength', '').removeClass('ng-valid-maxlength').removeClass('ng-valid').val('@fw$')
+    setTimeout(function(){
+        document.querySelector("#login_text_username").dispatchEvent(new Event('change'));
+        document.querySelector("#login_text_password").dispatchEvent(new Event('change'));
+        document.querySelector("#login_text_verifycode").dispatchEvent(new Event('change'));
+    })
 
     setTimeout(function () {
         $('#login_button_loginButton').trigger('click')
-    }, 100);
+    }, 200);
 }
 autoLogin()
+
